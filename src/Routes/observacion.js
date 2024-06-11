811053689851dc84056a0739b58128cfab63c12c
Phanesan <48665230+Phanesan@users.noreply.github.com>
2024-06-11 14:25:55 -0700

    cambios: remodelado de las rutas files

--- src/Routes/observacion.js
+++ src/Routes/observacion.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const authRoute = require('../middleware.js');
 const { Observaciones } = require('../models.js')
 const { Op } = require('sequelize');
+const { removeFile } = require('../AWS/S3Client.js')
 
 
 router.post('/', authRoute, async (req, res) => {
     const body = req.body;
+
     await Observaciones.create(body).then(data => {
         res.status(200).json({status:"ok",message:"Observación cargada a la base de datos"});
     }).catch(err => {
@@ -76,16 +78,34 @@ router.put('/', authRoute, async (req, res) => {
 
 router.delete('/:id', authRoute, async (req, res) => {
     const id = req.params.id;
+    let pathImg;
+    try {
+        pathImg = await Observaciones.findOne({where:{id:id}});
+        if(!pathImg) {
+            return res.status(200).json({status:"ok",message:"No se encontro una ID para eliminar"});
+        }
+    } catch (err) {
+        return res.status(200).json({status:"ok",message:"No se encontro una ID para eliminar"});
+    }
 
     await Observaciones.destroy({
         where: {
             id: id
         }
-    }).then(data => {
+    }).then(async data => {
         if(data === 0) {
             res.status(200).json({status:"ok",message:"No se encontro una ID para eliminar"})
         } else {
-            res.status(200).json({status:"ok",message:"Observación eliminada correctamente"});
+            if(pathImg.dataValues.imagenes.rutas.length == 1) {
+                await removeFile(pathImg.dataValues.imagenes.rutas[0])
+            } else if(pathImg.dataValues.imagenes.rutas.length > 1) {
+                const array = [...pathImg.dataValues.imagenes.rutas];
+                console.log(array)
+                await Promise.all(array.map(async data => {
+                    await removeFile(data);
+                }));
+            }
+            res.status(200).json({status:"ok",message:"Observación eliminada correctamente",filePath:pathImg.dataValues.imagenes.rutas});
         }
     }).catch(err => {
         res.status(400).json({status:"failed",message:"La API no puede procesar la solicitud",error_status:err})
