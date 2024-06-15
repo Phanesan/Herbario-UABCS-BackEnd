42b48acee2836f4649e097f7e8ff25dcd781fe20
Phanesan <48665230+Phanesan@users.noreply.github.com>
2024-06-14 22:26:04 -0700

    cambios: puerto agregado a las variables de entorno de los modelos

--- src/models.js
+++ src/models.js
@@ -9,6 +9,7 @@ require('dotenv').config();
  */
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_IP,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 });
 
